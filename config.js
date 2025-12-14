e0cc0e18ea947181606d55c93c33651571b0aa67
Harley Robert Thomas <try.nusrat@gmail.com>
2025-12-15 04:06:16 +0530

    Update API_BASE and GOOGLE_CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // Change this to your Cloud Run FastAPI URL when deployed
 // e.g., "https://gee-chlora-xxxxx-uc.a.run.app"
 window.APP_CONFIG = {
-  API_BASE: "http://localhost:8000",
-  GOOGLE_CLIENT_ID: "YOUR_CLIENT_ID.apps.googleusercontent.com"
+  API_BASE: "https://harleyrobertthomas.github.io/gee-chlorophyll-monitor/",
+  GOOGLE_CLIENT_ID: "732491930112-j17b0o60it9f1hvm2ie7b2r80gb2h5qe.apps.googleusercontent.com"
 };
